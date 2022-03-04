3aa71d1450b013e95935b1a9de238fa5ddcce53f
eliseu bento <eliseu1264@gmail.com>
2022-03-04 17:38:33 -0300

    comit de 04/03/2022

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/courses/model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
+
+
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'Front-end'},
+    {_id: '2', name: 'Angular2', category: 'Front-end2'},
+    {_id: '3', name: 'Angular3', category: 'Front-end3'},
+    {_id: '4', name: 'Angular4', category: 'Front-end4'}
+  ];
+
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+   //this.courses = [];
+   }
 
   ngOnInit(): void {
   }
